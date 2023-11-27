245c8d9b7c272182e23f916aafa3903f431578fe
Thomas Olinger <olingerthomas95@gmail.com>
2023-11-27 17:55:05 -0500

    Update App.tsx

--- BioVisionApp/src/App.tsx
+++ BioVisionApp/src/App.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import Sidebar from './Components/Menu';
+import Menu from './Components/Menu';
 
 const App: React.FC = () => {
   return (
     <div style={{ display: 'flex', height: '100vh' }}>
-      <Sidebar>
+      <Menu>
         {/* Your content for the left side goes here */}
-      </Sidebar>
+      </Menu>
       {/* The rest of your application content goes here */}
     </div>
   );
