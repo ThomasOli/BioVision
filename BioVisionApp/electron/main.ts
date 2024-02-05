88771c88fe516faf9d9306e9d85f2bb8ccdcbbec
Taebok Joseph Kim <josephkim7104@gmail.com>
2024-02-04 23:28:08 -0500

    Context Menu
    Added Context Menu with basic features

--- BioVisionApp/electron/main.ts
+++ BioVisionApp/electron/main.ts
@@ -1,7 +1,48 @@
 import { app, BrowserWindow, ipcMain, dialog } from 'electron';
 import * as path from 'path';
+import { download } from 'electron-dl';
+const contextMenu = require('electron-context-menu');
 
 let mainWindow: BrowserWindow | null;
+
+const template = [
+  { label: 'Minimize', click: () => mainWindow?.minimize() },
+  { label: 'Maximize', click: () => mainWindow?.maximize() },
+  { type: 'separator' },
+  { label: 'Copy', click: () => mainWindow?.webContents.copy() },
+  { label: 'Paste', click: () => mainWindow?.webContents.paste() },
+  { label: 'Delete', click: () => mainWindow?.webContents.delete() },
+  { type: 'separator' },
+  // {
+  //   label: 'Save Image',
+  //   visible: (params: { mediaType?: string }) => params.mediaType === 'image',
+  //   click: (menuItem: any, browserWindow: BrowserWindow, event: any) => {
+  //     const imageURL = event.srcURL;
+
+  //     if (imageURL) {
+  //       download(browserWindow, imageURL, {
+  //         saveAs: true,
+  //         directory: app.getPath('downloads'),
+  //       })
+  //         .then(dl => {
+  //           if (dl && !dl.getSavePath()) {
+  //             // The user canceled the download
+  //             console.log('Download canceled by the user.');
+  //           } else if (dl) {
+  //             console.log('Download completed:', dl.getSavePath());
+  //           }
+  //         })
+  //         .catch(error => {
+  //           console.error('Download error:', error);
+  //         });
+  //     }
+  //   },
+  // },
+  { type: 'separator' },
+  { label: 'Quit', click: () => app.quit() },
+];
+
+contextMenu({ prepend: () => template });
 
 function createWindow() {
   mainWindow = new BrowserWindow({
@@ -14,7 +55,6 @@ function createWindow() {
     },
   });
 
-  // Load the Vite application URL or build output
   const VITE_DEV_SERVER_URL = process.env.VITE_DEV_SERVER_URL;
 
   if (VITE_DEV_SERVER_URL) {
