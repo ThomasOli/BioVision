1df82b458e6192710f98024c10ecc837759cc3bc
RunningUpTheHill <tianzhong.j@ufl.edu>
2024-03-04 16:47:43 -0500

    Numbering

--- BioVisionApp/src/lib/point.ts
+++ BioVisionApp/src/lib/point.ts
@@ -1,15 +1,15 @@
 import { fabric } from "fabric";
-import FabricTool, { ConfigureCanvasProps } from "./fabrictool"
+import FabricTool, { ConfigureCanvasProps } from "./fabrictool";
 
 class PointTool extends FabricTool {
-  isMouseDown: boolean = false
-  fillColor: string = "#000000"
-  strokeWidth: number = 10
-  strokeColor: string = "#000000"
-  currentCircle: fabric.Circle = new fabric.Circle()
-  currentStartX: number = 0
-  currentStartY: number = 0
-  displayRadius: number = 1
+  isMouseDown: boolean = false;
+  fillColor: string = "#000000";
+  strokeWidth: number = 10;
+  strokeColor: string = "#000000";
+  currentCircle: fabric.Circle = new fabric.Circle();
+  currentStartX: number = 0;
+  currentStartY: number = 0;
+  displayRadius: number = 1;
 
   constructor(canvas: fabric.Canvas) {
     super(canvas);
@@ -20,46 +20,52 @@ class PointTool extends FabricTool {
   }
 
   configureCanvas({
-    strokeWidth,
-    strokeColor,
-    fillColor,
-    
-  }: ConfigureCanvasProps): () => void {
-    console.log(this._canvas)
+                    strokeWidth,
+                    strokeColor,
+                    fillColor,
 
-    this._canvas.isDrawingMode = false
-    this._canvas.selection = false
-    this._canvas.forEachObject((o) => (o.selectable = o.evented = false))
+                  }: ConfigureCanvasProps): () => void {
+    console.log(this._canvas);
 
-    this.strokeWidth = strokeWidth
-    this.strokeColor = strokeColor
-    this.fillColor = "#000000"
+    this._canvas.isDrawingMode = false;
+    this._canvas.selection = false;
+    this._canvas.forEachObject((o) => (o.selectable = o.evented = false));
 
-    this._canvas.on("mouse:down", (e: any) => this.onMouseDown(e))
-    this._canvas.on("mouse:move", (e: any) => this.onMouseMove(e))
-    this._canvas.on("mouse:up", (e: any) => this.onMouseUp(e))
-    this._canvas.on("mouse:out", (e: any) => this.onMouseOut(e))
+    this.strokeWidth = strokeWidth;
+    this.strokeColor = strokeColor;
+    this.fillColor = "#000000";
+
+    this._canvas.on("mouse:down", (e: any) => this.onMouseDown(e));
+    this._canvas.on("mouse:move", (e: any) => this.onMouseMove(e));
+    this._canvas.on("mouse:up", (e: any) => this.onMouseUp(e));
+    this._canvas.on("mouse:out", (e: any) => this.onMouseOut(e));
     return () => {
       //@ts-ignore
-      this._canvas.off("mouse:down")
+      this._canvas.off("mouse:down");
       //@ts-ignore
-      this._canvas.off("mouse:move")
+      this._canvas.off("mouse:move");
       //@ts-ignore
-      this._canvas.off("mouse:up")
+      this._canvas.off("mouse:up");
       //@ts-ignore
-      this._canvas.off("mouse:out")
-    }
-  
+      this._canvas.off("mouse:out");
+    };
   }
-  
+
   onMouseDown(o: any) {
-    console.log("clicked")
-    let canvas = this._canvas
-    let _clicked = o.e["button"]
-    this.isMouseDown = true
-    let pointer = canvas.getPointer(o.e)
-    this.currentStartX = pointer.x - (this.strokeWidth / 2.)
-    this.currentStartY = pointer.y //- (this._minRadius + this.strokeWidth)
+    let canvas = this._canvas;
+    let _clicked = o.e["button"];
+    this.isMouseDown = true;
+    let pointer = canvas.getPointer(o.e);
+    this.currentStartX = pointer.x - (this.strokeWidth / 2.);
+    this.currentStartY = pointer.y;
+    let pointNum = canvas.getObjects().filter(obj => obj.type === 'circle').length + 1;
+    let text = new fabric.Text(pointNum.toString(), {
+      left: this.currentStartX + 10,
+      top: this.currentStartY - 10,
+      fontSize: 10,
+      selectable: false,
+      evented: false,
+    });
     this.currentCircle = new fabric.Circle({
       left: this.currentStartX,
       top: this.currentStartY,
@@ -71,36 +77,28 @@ class PointTool extends FabricTool {
       selectable: false,
       evented: false,
       radius: 2,
-    })
+    });
     if (_clicked === 0) {
-      
-
-      canvas.add(this.currentCircle)
-      this._canvas.renderAll(); 
+      canvas.add(this.currentCircle, text);
+      canvas.renderAll();
     }
   }
 
   onMouseMove(o: any) {
-        console.log("clicked")
-
-    if (!this.isMouseDown) return
-    let canvas = this._canvas
-    this.currentCircle.setCoords()
-    canvas.renderAll()
+    if (!this.isMouseDown) return;
+    let canvas = this._canvas;
+    this.currentCircle.setCoords();
+    canvas.renderAll();
   }
 
   onMouseUp(o: any) {
-    console.log("clicked")
-
-    this.isMouseDown = false
+    this.isMouseDown = false;
   }
 
   onMouseOut(o: any) {
-    console.log("clicked")
-
-    this.isMouseDown = false
+    this.isMouseDown = false;
   }
 
 }
 
-export default PointTool
+export default PointTool;
