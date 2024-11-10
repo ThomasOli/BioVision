629e45c19d9fecd8cd5df88d70db52ccfb1768e7
TheRealAryanTiwari <aryanstiwari24@gmail.com>
2024-11-09 23:07:26 -0500

    moved image to left

--- src/Components/ImageLabeler.tsx
+++ src/Components/ImageLabeler.tsx
@@ -170,7 +170,7 @@ const ImageLabeler: React.FC<ImageLabelerProps> = ({
               border: "1px solid gray",
               backgroundColor: "#f0f0f0",
               cursor: "crosshair",
-              marginRight: "auto",
+              marginRight: "20%",
             }}
             scaleX={scale}
             scaleY={scale}
